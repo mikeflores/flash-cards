29aa9ae654148c954553ba07da655b5e87a8fa29
Mike Flores <michaelprflores@gmail.com>
2016-09-20 14:39:19 -0400

    Session with Jesse on how to skip through cards

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -19,18 +19,36 @@ var deck = {
 var vader =  deck.cards[0]
 // =====================================
 
-
 $(document).ready(function(){
     $("h1").html(deck.deckName)
-    $("dt").html(deck.cards[0].term)
-    $(".definition").html(deck.cards[0].definition).hide()
+    showCard(0)
     $(".mark-as").hide()
     // console.log("Document Ready")
+    // for (i = 0; i < deck.length; i++) {
+    //     $(".card").append("<div class='.card'></div>")
+    // }
+
 })
 
-$(".card").on("click", function(event){
+$(".card dt, .card dd").on("click", function(event){
     $(".definition").toggle()
-    $("dt").html(deck.cards[0].term).toggle()
+    $("dt").toggle()
     $(".mark-as").toggle()
     // console.log("works")
 })
+
+$(".js-skip").on("click", function(event) {
+    console.log("here")
+    event.preventDefault()
+    var index = $(".card").attr("data-index")
+    showCard(parseInt(index)+1)
+    console.log(index)
+})
+
+function showCard(index){
+    // debugger
+    $(".definition").html(deck.cards[index].definition).hide()
+    $("dt").html(deck.cards[index].term)
+    $(".card").attr("data-index", index)
+    $(".card-number").html(index+1)
+}
