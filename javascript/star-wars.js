c8f3e91ff3f24fb568422bba9c350ca7c11917b9
Mike Flores <michaelprflores@gmail.com>
2016-09-21 12:06:51 -0400

    looks like things are working. Thanks Jesse

--- javascript/star-wars.js
+++ javascript/star-wars.js
@@ -17,6 +17,10 @@ var deck = {
         {
             term: "Obi Wan Kenobi", 
             definition: "Probably the most well written character in the Star Wars canon." 
+        },
+        {
+            term: "Palpatine", 
+            definition: "An evil dude that rules the Empire." 
         }
     ]
 }
@@ -25,51 +29,64 @@ var deck = {
 $(document).ready(function(){
     $(".deckName").html(deck.deckName) // fills in the name of the deck for this page
     showCard(0) // starts the deck at card number 1
-    $(".mark-as").hide() // hides the grading system
 })
 
 $(".card dt, .card dd").on("click", function(event){
-    $("dd").toggle() // event that allows for toggling
-    $("dt").toggle()
-    // $(".mark-as").toggle()
+    $("dd, dt, .grade").toggle() // event that allows for toggling
 })
 
-// event listener to get to the previous card
+// event listener to go to the previous card
 $(".previous").on("click", function(event) {
     event.preventDefault()
     var index = $(".card").attr("data-index")
-    showCard(parseInt(index)-1) // not sure what this does
+    showCard(parseInt(index)-1) // still vague on what this does
 })
 
-// event listener to get to the next card
+// event listener to go to the next card
 $(".next").on("click", function(event) {
-    event.preventDefault()
-    var index = $(".card").attr("data-index")
-    showCard(parseInt(index)+1) // not sure what this does
+    next()
 })
 
 
-// if no more cards call myScore
-    var numRight = 0
-    $(".right").on("click", function(event) {
-        event.preventDefault()
-        numRight++
-        console.log(numRight)
-    })
-    var numWrong = 0
-    $(".right").on("click", function(event) {
-        event.preventDefault()
-        numWrong++
-        console.log(numWrong)
-    })
+var numRight = 0
+$(".right").on("click", function(event) {
+    numRight++
+    next()
+})
 
+var numWrong = 0
+$(".wrong").on("click", function(event) {
+    event.preventDefault()
+    numWrong++
+    next()
+})
+
+
+// Functions
 function showCard(index){
-    // debugger
-    $("dd").html(deck.cards[index].definition).hide() // fills in the definition and hides it
-    $("dt").html(deck.cards[index].term) // fills in the term
+
+    if (deck.cards[index]){
+        var definition = deck.cards[index].definition
+        var term = deck.cards[index].term
+    } else {
+        var term = "You got " + numRight + " correct " + "and " + numWrong + " wrong!"
+        index = -1
+        $(".next").html("start over")
+        $(".status, .previous").hide()
+        var definition = "These aren't the droids you're looking for!"
+    }
+    $("dd").html(definition).hide() // fills in the definition and hides it
+    $("dt").html(term).show() // fills in the term
+    $(".grade").hide()
     $(".card").attr("data-index", index)
     $(".card-number").html(index+1) // fills in the card order
     $(".number-of-cards").html(deck.cards.length) // fills in the total number of cards in the deck
+}
+
+function next() {
+    event.preventDefault()
+    var index = $(".card").attr("data-index")
+    showCard(parseInt(index)+1)
 }
 
 // at the end viewScore
